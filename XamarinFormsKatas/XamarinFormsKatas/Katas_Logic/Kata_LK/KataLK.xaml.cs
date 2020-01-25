85a25fad54cf1270818046959f68c890e1e0cccb
Jose <jmmontero.ortega@gmail.com>
2020-01-25 09:08:32 +0100

    Add latest Katas

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LK/KataLK.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LK
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class KataLK : ContentPage
+	{
+		public KataLK ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
