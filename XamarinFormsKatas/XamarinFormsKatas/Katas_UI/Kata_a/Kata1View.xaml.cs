d94cf02e05b100804b0a3ca9977abf9387145921
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2020-01-25 20:04:01 +0100

    UI A finalizado.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_a/Kata1View.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_a/Kata1View.xaml.cs
@@ -15,6 +15,81 @@ namespace XamarinFormsKatas.Katas.Kata1
 		public Kata1View ()
 		{
 			InitializeComponent ();
-		}
+            buttonB.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_b.KataB());
+            };
+            buttonC.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_c.KataC());
+            };
+            buttonD.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_d.KataD());
+            };
+            buttonE.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_e.KataE());
+            };
+            buttonF.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_f.KataF());
+            };
+            buttonG.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_g.KataG());
+            };
+            buttonH.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_h.KataH());
+            };
+            buttonI.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_i.KataI());
+            };
+            buttonJ.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_j.KataJ());
+            };
+            buttonK.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_k.KataK());
+            };
+            buttonL.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_l.KataL());
+            };
+            buttonM.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_m.KataM());
+            };
+            buttonN.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_n.KataN());
+            };
+            buttonO.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_o.KataO());
+            };
+            buttonP.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_p.KataP());
+            };
+            buttonQ.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_q.KataQ());
+            };
+            buttonR.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_r.KataR());
+            };
+            buttonS.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_s.KataS());
+            };
+            buttonT.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_t.KataT());
+            };
+            buttonU.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_u.KataU());
+            };
+            buttonV.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_v.KataV());
+            };
+            buttonW.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_w.KataW());
+            };
+            buttonX.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_x.KataX());
+            };
+            buttonY.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_y.KataY());
+            };
+            buttonZ.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Katas_UI.Kata_z.KataZ());
+            };
+        }
 	}
 }
---
+++ XamarinFormsKatas/XamarinFormsKatas/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormsKatas.Katas.Kata1;
+
+namespace XamarinFormsKatas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            buttonInicio.Clicked += (sender, e) => {
+                Navigation.PushAsync(new Kata1View());
+            };
+        }
+    }
+}
