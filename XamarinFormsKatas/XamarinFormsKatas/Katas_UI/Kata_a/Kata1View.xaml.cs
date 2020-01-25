5832db4668cc5c5970adaacb7d5ef2d6a88e8018
Jose <jmmontero.ortega@gmail.com>
2020-01-25 07:58:17 +0100

    Added exercises relation and first view

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_a/Kata1View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsKatas.Katas.Kata1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Kata1View : ContentPage
+	{
+		public Kata1View ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
