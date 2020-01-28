eacdf39bbef27ea5a4c72a8b79267b4388259417
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2020-01-28 13:40:43 +0100

    UI U Finalizado.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_u/KataU.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_u/KataU.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace XamarinFormsKatas.Katas_UI.Kata_u
@@ -15,6 +17,20 @@ namespace XamarinFormsKatas.Katas_UI.Kata_u
 		public KataU ()
 		{
 			InitializeComponent ();
-		}
+            var posicion1 = new Position(37.356316, -5.981843); 
+            var posicion2 = new Position(37.371638, -5.999653);
+            var pin = new Pin {
+                Type = PinType.Place,
+                Position = posicion1,
+                Label="Campo Benito Villamarín"
+            };
+            var pin2 = new Pin
+            {
+                Type = PinType.Place,
+                Position = posicion2,
+                Label = "Fesac"
+            };
+            MapView.Pins.Add(pin);
+        }
 	}
 }
