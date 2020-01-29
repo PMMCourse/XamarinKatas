e63bd35b7f0135f026e36468213f040edfbb3320
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2020-01-29 13:06:36 +0100

    UI W Finalizado.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
@@ -16,5 +16,28 @@ namespace XamarinFormsKatas.Katas_UI.Kata_w
 		{
 			InitializeComponent ();
 		}
+        private void sendButton(object sender, EventArgs e) {
+            entryControl.IsVisible = false;
+            labelControl.IsVisible = false;
+            buttonControl.IsVisible = false;
+            activityControl.IsVisible = false;
+
+            if (switchEntry.IsToggled) {
+                entryControl.IsVisible= true;
+            }
+            if (switchLabel.IsToggled)
+            {
+                labelControl.IsVisible = true;
+            }
+            if (switchButton.IsToggled)
+            {
+                buttonControl.IsVisible = true;
+            }
+            if (switchActivity.IsToggled)
+            {
+                activityControl.IsVisible = true;
+            }
+            Navigation.PushAsync(new Katas_UI.Kata_w.elementoControles(entryControl, labelControl, buttonControl,activityControl));
+        }
 	}
 }
