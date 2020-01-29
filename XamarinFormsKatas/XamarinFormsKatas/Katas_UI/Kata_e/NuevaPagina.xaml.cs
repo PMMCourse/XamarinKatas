457a21a63e04a2a68a98b5070e3d776848720ddf
Emmanuel <emmanuel@emmanuel.es>
2020-01-29 20:09:08 +0100

    Kata_e completado.

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_e/NuevaPagina.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_e
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NuevaPagina : ContentPage
+    {
+        public NuevaPagina(Persona persona)
+        {
+            InitializeComponent(); 
+            labelTexto.Text = persona.ToString();
+            imagen.Source = persona.Foto;
+
+        }
+    }
+}
