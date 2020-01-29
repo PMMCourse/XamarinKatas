c6e60ae17f4f6dc49e3a31efeebba24fefae2a2b
Emmanuel <emmanuel@emmanuel.es>
2020-01-29 15:12:01 +0100

    Farmeo de botones completado.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_a/KataA.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_a/KataA.xaml.cs
@@ -17,9 +17,149 @@ namespace XamarinFormsKatas.Katas_UI.Kata_a
             InitializeComponent();
 
         }
+        //Botones IU
         public void BotonKata_b(object sender, EventArgs args)
         {
-            Page p = new NavigationPage(new Kata_b.KataB()); 
+            Navigation.PushAsync(new Katas_UI.Kata_b.KataB());
         }
+        public void BotonKata_c(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_c.KataC());
+        }
+        public void BotonKata_d(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_d.KataD());
+        }
+        public void BotonKata_e(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_e.KataE());
+        }
+        public void BotonKata_f(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_f.KataF());
+        }
+        public void BotonKata_g(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_g.KataG());
+        }
+        public void BotonKata_h(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_h.KataH());
+        }
+        public void BotonKata_i(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_i.KataI());
+        }
+        public void BotonKata_j(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_j.KataJ());
+        }
+        public void BotonKata_k(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_k.KataK());
+        }
+        public void BotonKata_l(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_l.KataL());
+        }
+        public void BotonKata_m(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_m.KataM());
+        }
+        public void BotonKata_n(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_n.KataN());
+        }
+        public void BotonKata_o(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_o.KataO());
+        }
+        public void BotonKata_p(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_p.KataP());
+        }
+        public void BotonKata_q(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_q.KataQ());
+        }
+        public void BotonKata_r(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_r.KataR());
+        }
+        public void BotonKata_s(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_s.KataS());
+        }
+        public void BotonKata_t(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_t.KataT());
+        }
+        public void BotonKata_u(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_u.KataU());
+        }
+        public void BotonKata_v(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_v.KataV());
+        }
+        public void BotonKata_w(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_w.KataW());
+        }
+        public void BotonKata_y(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_y.KataY());
+        }
+        public void BotonKata_z(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_UI.Kata_z.KataZ());
+        }
+
+        //Botones Logic
+        public void BotonKata_la(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LA.KataLA());
+        }
+        public void BotonKata_lb(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LB.KataLB());
+        }
+        public void BotonKata_lc(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LC.KataLC());
+        }
+        public void BotonKata_ld(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LD.KataLD());
+        }
+        public void BotonKata_le(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LE.KataLE());
+        }
+        public void BotonKata_lf(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LF.KataLF());
+        }
+        public void BotonKata_lg(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LG.KataLG());
+        }
+        public void BotonKata_lh(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LH.KataLH());
+        }
+        public void BotonKata_li(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LI.Kata_LI());
+        }
+        public void BotonKata_lj(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LJ.Kata_LJ());
+        }
+        public void BotonKata_lk(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Katas_Logic.Kata_LK.KataLK());
+        }
+
     }
 }
