48685d66af4345bf9ef28927596b5f8a2c6511c4
Emmanuel <emmanuel@emmanuel.es>
2020-01-29 19:15:19 +0100

    Kata_d completado.

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_d/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_d
+{
+   public class Persona
+    {
+        public Persona(string nombre, string edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public string Nombre { get; set; }
+            public string Edad { get; set; }
+            public override string ToString()
+            {
+                return Nombre;
+            }
+        }
+    }
+
