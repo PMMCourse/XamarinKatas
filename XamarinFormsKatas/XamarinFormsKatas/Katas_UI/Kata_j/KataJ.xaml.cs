0a97ad17eb62627f6ccc3b28aabb420d20ae55eb
Natalia <Natalia@NATALIA-HP>
2020-01-30 17:36:36 +0100

    Ejer J

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_j/KataJ.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_j/KataJ.xaml.cs
@@ -9,12 +9,29 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinFormsKatas.Katas_UI.Kata_j
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class KataJ : ContentPage
-	{
-		public KataJ ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KataJ : ContentPage
+    {
+        public KataJ()
+        {
+            InitializeComponent();
+            bool denegado = false;
+            login.Clicked += (sender, e) =>
+            {
+                if((entrada2.Text==null|| entrada2.Text == "") && denegado == false)
+                {
+                    Entry entrada = new RoundedEntrada();
+                    stack1.Children.Remove(entrada2);
+                    stack1.Children.Add(entrada1);
+                    denegado = true;
+                    var label = new Label()
+                    {
+                        Text = "Ha salido un ERROR"
+                    };
+                stack1.Children.Add(label);
+                }
+            };
+           
+        }
+    }  
 }
