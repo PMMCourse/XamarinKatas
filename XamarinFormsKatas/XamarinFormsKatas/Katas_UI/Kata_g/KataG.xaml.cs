b2ac3e29dbe8e427faadcd6d4e71f4bc211e0c50
Emmanuel <emmanuel@emmanuel.es>
2020-01-30 20:02:36 +0100

    Kata_g completada.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_g/KataG.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_g/KataG.xaml.cs
@@ -12,9 +12,15 @@ namespace XamarinFormsKatas.Katas_UI.Kata_g
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataG : ContentPage
 	{
-		public KataG ()
+		public KataG()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+		}
+		public void BotonKata_g(object sender, EventArgs args)
+		{
+			//si lo pongo con , da simplemente la media vuelta
+			botonDinamico.RotateTo(180 , 360);
+			//si lo pongo asi: botonDinamico.RotateTo(180 * 360); vuelve a su estado original despues de girar
 		}
 	}
 }
