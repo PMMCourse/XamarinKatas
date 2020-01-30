4dcf2fee73a893f61cabc80002b27d27f8ed2c24
Natalia <Natalia@NATALIA-HP>
2020-01-30 18:41:11 +0100

    Ejer K-L-M

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_k/KataK.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_k/KataK.xaml.cs
@@ -12,9 +12,31 @@ namespace XamarinFormsKatas.Katas_UI.Kata_k
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataK : ContentPage
 	{
+        Random numAlea= new Random();
+        List<int> listaNumeros = new List<int>();
+
 		public KataK ()
 		{
 			InitializeComponent ();
+            LeerDatos();
+            PullRefresh.RefreshCommand = new Command(() =>
+             {
+                 PullRefresh.IsRefreshing = true;
+                 PullRefresh.IsRefreshing = true;
+                 LeerDatos();
+             });
 		}
-	}
+
+        private void LeerDatos()
+        {
+            PullRefresh.ItemsSource = null;
+            listaNumeros.Clear();
+            for (int i = 0; i < 9; i++)
+            {
+                listaNumeros.Add(numAlea.Next());
+            }
+            PullRefresh.ItemsSource = listaNumeros;
+            PullRefresh.IsRefreshing = false;
+        }
+    }
 }
