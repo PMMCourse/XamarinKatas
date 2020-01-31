424393f38f4bbf1f51e734bec37dc8249f984e17
Emmanuel <emmanuel@emmanuel.es>
2020-01-31 18:25:26 +0100

    katas_J Completado.

---
+++ XamarinFormsKatas/XamarinFormsKatas.UWP/MiRender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using XamarinFormsKatas.UWP;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(MiRender))]
+namespace XamarinFormsKatas.UWP
+{
+    public class MiRender : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            try
+            {//Si el texto cambia en cualquier momento se lanza el evento
+                Control.TextChanged += Control_TextChanged;
+            }
+            catch (System.ArgumentException)
+            {
+               new XamarinFormsKatas.MainPage();
+            }
+            
+
+        }
+
+        private void Control_TextChanged(object sender, Windows.UI.Xaml.Controls.TextChangedEventArgs e)
+        {//se comprueba que sea o no nulo el contenido del texto y en funcion a eso se cambia el color del borde
+            if (string.IsNullOrWhiteSpace(Control.Text))
+            {
+                Control.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {// restauro el color normal de windows
+                Control.BorderBrush = new SolidColorBrush(Colors.DodgerBlue);
+            }
+        }
+    }
+}
--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_j/KataJ.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_j/KataJ.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +11,24 @@ namespace XamarinFormsKatas.Katas_UI.Kata_j
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataJ : ContentPage
 	{
-		public KataJ ()
+		public KataJ()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+			//Mira MiRender
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+
+			if (!(String.IsNullOrEmpty(inputUsuario.Text)))
+			{
+				DisplayAlert("Login", "se logueo", "ok");
+				labelError.IsVisible = false;
+			}
+			else
+			{
+				labelError.IsVisible = true;
+			}
 		}
 	}
 }
