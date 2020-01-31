8704b189db13fb80336c07b71cfbc52ad4279340
Natalia <Natalia@NATALIA-HP>
2020-01-31 17:04:25 +0100

    Ejer R

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_r/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_r
+{
+    public class Cliente
+    {
+        public string nombre { get; set; }
+        public string  dni { get; set; }
+        public Cliente(string nombre,string dni)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+        }
+        public Cliente()
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Cliente: {nombre}, dni: {dni} ";
+        }
+    }
+}
---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_r/proveedores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_r
+{
+    public class proveedores
+    {
+        public string  nombre { get; set; }
+        public string Producto { get; set; }
+        public proveedores(String nombre,string producto)
+        {
+            this.nombre = nombre;
+            this.Producto = producto;
+        }
+        public proveedores()
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"proveedor: {nombre}, producto: {Producto}";
+
+        }
+    }
+}
