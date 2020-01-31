14cda3a96190553fad023f018b046110c2bee436
Natalia <Natalia@NATALIA-HP>
2020-01-31 16:36:47 +0100

    Ejer Q

---
+++ XamarinFormsKatas/XamarinFormsKatas.Android/EntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace XamarinFormsKatas.Droid
+{
+    public class EntryRenderer:Entry
+    {
+        public static BindableProperty PropertythicknessBorder = BindableProperty.Create(nameof(thicknessBorder), typeof(int), typeof(EntryRenderer), 0);
+        public static BindableProperty PropertyColorBorder = BindableProperty.Create(nameof(ColorBorder), typeof(Color), typeof(EntryRenderer), Color.Transparent);
+
+        public EntryRenderer(Context contexto)
+        {
+        }
+
+        public Color ColorBorder
+        {
+            get => (Color)GetValue(PropertyColorBorder);
+            set => SetValue(PropertyColorBorder, value);
+        }
+        public int thicknessBorder
+        {
+            get => (int)GetValue(PropertythicknessBorder);
+            set => SetValue(PropertythicknessBorder,value);
+        }
+    }
+}
---
+++ XamarinFormsKatas/XamarinFormsKatas.Android/entryBorderRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinFormsKatas.Droid;
+using XamarinFormsKatas.Katas_UI.Kata_q.Controls;
+[assembly:ExportRenderer(typeof(Xamarin.Forms.Platform.Android.EntryRenderer),typeof(entryBorderRenderer))]
+namespace XamarinFormsKatas.Droid
+{
+    public class entryBorderRenderer: EntryRenderer
+    {
+        public entryBorderRenderer(Context contexto) : base(contexto)
+        {
+            
+        }
+        protected override void CambioElemento(ElementChangedEventArgs<Entry> e)
+        {
+            var element = new GradientDrawable();
+            element.SetShape(ShapeType.Rectangle);
+            element.SetStroke(0, Android.Graphics.Color.Transparent);
+            Control.setBackgroung(element);
+        }
+    }
+}
