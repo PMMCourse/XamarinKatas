e4b5b823cbb5fa93868af65596d3af828afd8f8a
Daniel Albarracin <daniel.fesac@hotmail.com>
2020-01-31 13:15:19 +0100

    Parte LA. BaseViewModel

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LA/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LA
+{
+    public class BaseViewModel : BindableObject
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
