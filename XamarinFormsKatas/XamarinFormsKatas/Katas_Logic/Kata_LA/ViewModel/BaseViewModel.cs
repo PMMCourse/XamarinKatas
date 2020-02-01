49062cefc49bdb8d3fa0bbe216ab2d2d37a0cfb6
nacho190 <nacho190@gmail.com>
2020-02-01 19:39:28 +0100

    Logica a

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LA/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LA.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
