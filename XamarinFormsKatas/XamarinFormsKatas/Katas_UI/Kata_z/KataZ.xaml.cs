61af2e129b3d9cdc00a687feb90c526ab3f84578
nacho190 <nacho190@gmail.com>
2020-02-01 19:14:11 +0100

    UI z

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_z/KataZ.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_z/KataZ.xaml.cs
@@ -42,29 +42,33 @@ namespace XamarinFormsKatas.Katas_UI.Kata_z
 
             Grid grid = new Grid()
             {
+                BackgroundColor = Color.Blue,
+                HeightRequest = 500,
+                WidthRequest = 500
             };
             grid.RowDefinitions = new RowDefinitionCollection();
             grid.ColumnDefinitions = new ColumnDefinitionCollection();
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 7; i++)
             {
                 grid.RowDefinitions.Add(new RowDefinition());
             }
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 3; i++)
             {
                 grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
             Sl.Children.Add(grid);
             int cont = 0;
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 7; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     if (cont < 20)
                     {
                         grid.Children.Add(new Label
                         {
                             Text = nombres[cont],
+                            FontSize = 24,
                             HorizontalOptions = LayoutOptions.Center
                         }, j, i);
                         cont++;
