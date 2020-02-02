f3554a3cba55129c553744dca89e902bb9cc1d25
Emmanuel <emmanuel@emmanuel.es>
2020-02-02 20:26:21 +0100

    Kata_q completado.

---
+++ XamarinFormsKatas/XamarinFormsKatas.UWP/RenderKataQ.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using XamarinFormsKatas.UWP;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(RenderKataQ))]
+namespace XamarinFormsKatas.UWP
+{
+    public class RenderKataQ : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.BorderBrush = new SolidColorBrush(Colors.Transparent);
+
+        }
+    }
+}
