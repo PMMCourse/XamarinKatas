5f3f7c5deb0157e9240a8ffc90fd20282ba2adb8
Natalia <Natalia@NATALIA-HP>
2020-02-03 15:50:32 +0100

    Ejer Y

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_y/KataY.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_y/KataY.xaml.cs
@@ -12,9 +12,65 @@ namespace XamarinFormsKatas.Katas_UI.Kata_y
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataY : ContentPage
 	{
+        public List<string> lista = new List<string>();
+
 		public KataY ()
 		{
 			InitializeComponent ();
-		}
+
+
+            lista.Add("luis");
+            lista.Add("franco");
+            lista.Add("loren");
+            lista.Add("enrique");
+            lista.Add("pepe");
+            lista.Add("marina");
+            lista.Add("irene");
+            lista.Add("julia");
+            lista.Add("lourdes");
+            lista.Add("inma");
+            lista.Add("guillermo");
+            lista.Add("mateo");
+            lista.Add("maria");
+            lista.Add("armando");
+            lista.Add("manolo");
+            lista.Add("manuel");
+            lista.Add("luis");
+            lista.Add("gregorio");
+            lista.Add("luisita");
+            lista.Add("amelia");
+
+            Grid nuevoGrid = new Grid()
+            {
+
+            };
+            nuevoGrid.RowDefinitions = new RowDefinitionCollection();
+            nuevoGrid.ColumnDefinitions = new ColumnDefinitionCollection();
+
+            for(int i = 0; i < 3; i++)
+            {
+                nuevoGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                nuevoGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            sl1.Children.Add(nuevoGrid);
+            int contador = 0;
+            for (int i = 0;i<3;i++) {
+                for(int j = 0; j < 8; j++)
+                {
+                    if (contador < 20)
+                    {
+                        nuevoGrid.Children.Add(new Label
+                        {
+                            Text = lista[contador],
+                            HorizontalOptions=LayoutOptions.Center
+                        },j,i);
+                        contador++;
+                    }
+                }
+            }
+        }
 	}
 }
