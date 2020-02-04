20e505247f62782335cfb1a730c2471fa2c7a565
Emmanuel <emmanuel@emmanuel.es>
2020-02-04 23:02:38 +0100

    kata_W completado;

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +11,39 @@ namespace XamarinFormsKatas.Katas_UI.Kata_w
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataW : ContentPage
 	{
-		public KataW ()
+		public StackLayout sl = new StackLayout();
+		public KataW()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+
+			BotonNavegar.Clicked += Button_Navegar;
+		}
+
+		private void Button_Navegar(object sender, EventArgs e)
+		{
+			
+			if (switchEntry.IsToggled)
+				{
+				sl.Children.Add(entryVist1);
+				}
+			
+			if (switchLabel.IsToggled)
+				{
+				sl.Children.Add(labVist1);
+				}
+				
+			if (switchBoton.IsToggled)
+				{
+				sl.Children.Add(buttVist1);
+				}
+					
+			if (switchAIndicator.IsToggled)
+				{
+				sl.Children.Add(AIvist1);
+				}
+
+			//le paso el stacklayout con los componentes que han sido marcados
+			Navigation.PushAsync(new Katas_UI.Kata_w.SegundaVista(sl));
 		}
 	}
 }
