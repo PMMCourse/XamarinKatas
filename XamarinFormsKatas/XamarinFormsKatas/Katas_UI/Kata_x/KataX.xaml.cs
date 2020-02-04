4c9442b5086be5cf5232b79747bb964af100648b
Fran <frasiecue@gmail.com>
2020-02-04 14:41:45 +0100

    Ejercicio X

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_x/KataX.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_x/KataX.xaml.cs
@@ -12,9 +12,19 @@ namespace XamarinFormsKatas.Katas_UI.Kata_x
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataX : ContentPage
 	{
+		List<string> listaNombres = new List<string>();
 		public KataX ()
 		{
 			InitializeComponent ();
+			listaNombres.Add("Fran");
+			listaNombres.Add("Juan");
+			listaNombres.Add("Fernando");
+			Entry1.Focused += HacerVisible;
+		}
+
+		private void HacerVisible(Object sender, EventArgs e) {
+			Picker1.ItemsSource = listaNombres;
+			Picker1.IsVisible = true;
 		}
 	}
 }
