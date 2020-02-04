bc1b088f5ece3c9fba89c9a6dfe909f538a460c7
Fran <frasiecue@gmail.com>
2020-02-04 14:15:40 +0100

    Alex Dunn (https://alexdunn.org/2017/03/08/xamarin-forms-borderless-entry/) patrocina el ejercicio Q.

---
+++ XamarinFormsKatas/XamarinFormsKatas.Android/BorderlessEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinFormsKatas.Droid;
+using XamarinFormsKatas.Katas_UI.Kata_q;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+
+namespace XamarinFormsKatas.Droid {
+    [Obsolete]
+    public class BorderlessEntryRenderer : EntryRenderer {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e) {
+            base.OnElementChanged(e);
+            if (e.OldElement == null) {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ XamarinFormsKatas/XamarinFormsKatas.iOS/BorderlessEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormsKatas.iOS;
+using XamarinFormsKatas.Katas_UI.Kata_q;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace XamarinFormsKatas.iOS {
+
+    public class BorderlessEntryRenderer : EntryRenderer {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+
+}
