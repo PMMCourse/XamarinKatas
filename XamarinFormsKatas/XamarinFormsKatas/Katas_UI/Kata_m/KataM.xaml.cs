93a2966d1750f106c00172ea7e24210da8f13c6f
Fran <frasiecue@gmail.com>
2020-02-04 17:26:45 +0100

    Ejercicio M.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_m/KataM.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_m/KataM.xaml.cs
@@ -7,14 +7,17 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace XamarinFormsKatas.Katas_UI.Kata_m
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class KataM : ContentPage
-	{
-		public KataM ()
-		{
-			InitializeComponent ();
-		}
-	}
+namespace XamarinFormsKatas.Katas_UI.Kata_m {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KataM : ContentPage {
+        public KataM() {
+            InitializeComponent();
+            buttonAdd.Clicked += (sender, e) => {
+                var label = new Label() {
+                    Text = "Nuevo label",
+                };
+                StackLabel.Children.Add(label);
+            };
+        }
+    }
 }
