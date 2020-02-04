783a4b0ef6a3754ced13396bf4514c3326b898ad
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2020-02-04 15:36:00 +0100

    Kata Logic LH Mejorado.

--- XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LH/View/Page1.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LH/View/Page1.xaml.cs
@@ -13,37 +13,10 @@ namespace XamarinFormsKatas.Katas_Logic.Kata_LH.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
-        Boolean _entrySwitch=false;
-        Boolean _labelSwitch = false;
-        Boolean _buttonSwitch = false;
-        Boolean _activitySwitch = false;
         public Page1(Boolean _entrySwitch, Boolean _labelSwitch, Boolean _buttonSwitch, Boolean _activitySwitch)
         {
             InitializeComponent();
-            BindingContext = new SecondViewModel();
-            this._entrySwitch = _entrySwitch;
-            this._labelSwitch = _labelSwitch;
-            this._buttonSwitch = _buttonSwitch;
-            this._activitySwitch = _activitySwitch;
-            controlState();
-        }
-
-        private void controlState()
-        {
-            Entry entryControl = new Entry();
-            entryControl.IsVisible=_entrySwitch;
-            Label labelControl = new Label();
-            labelControl.IsVisible = _labelSwitch;
-            Button buttonControl = new Button();
-            buttonControl.IsVisible = _buttonSwitch;
-            ActivityIndicator activityControl = new ActivityIndicator();
-            activityControl.IsVisible = _activitySwitch;
-            activityControl.IsEnabled = true;
-            activityControl.IsRunning = true;
-            stackControl.Children.Add(entryControl);
-            stackControl.Children.Add(labelControl);
-            stackControl.Children.Add(buttonControl);
-            stackControl.Children.Add(activityControl);
+            BindingContext = new SecondViewModel(_entrySwitch, _labelSwitch, _buttonSwitch, _activitySwitch);
         }
     }
 }
---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LH/ViewModel/SecondViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LH.ViewModel
+{
+    public class SecondViewModel : BindableObject
+    {
+        public SecondViewModel(Boolean _entrySwitch, Boolean _labelSwitch, Boolean _buttonSwitch, Boolean _activitySwitch)
+        {
+            this._entrySwitch = _entrySwitch;
+            this._labelSwitch = _labelSwitch;
+            this._buttonSwitch = _buttonSwitch;
+            this._activitySwitch = _activitySwitch;
+        }
+
+        private Boolean _entrySwitch = false;
+
+        public Boolean EntrySwitch
+        {
+            get => _entrySwitch;
+            set
+            {
+                OnPropertyChanged();
+                _entrySwitch = value;
+            }
+        }
+        private Boolean _labelSwitch = false;
+
+        public Boolean LabelSwitch
+        {
+            get => _labelSwitch;
+            set
+            {
+                OnPropertyChanged();
+                _labelSwitch = value;
+            }
+        }
+        private Boolean _buttonSwitch = false;
+
+        public Boolean ButtonSwitch
+        {
+            get => _buttonSwitch;
+            set
+            {
+                OnPropertyChanged();
+                _buttonSwitch = value;
+            }
+        }
+        private Boolean _activitySwitch = false;
+
+        public Boolean ActivitySwitch
+        {
+            get => _activitySwitch;
+            set
+            {
+                OnPropertyChanged();
+                _activitySwitch = value;
+            }
+        }
+    }
+}
