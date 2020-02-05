360948c91dbb1e78657ab7a10c84d30c78341bdc
Natalia <Natalia@NATALIA-HP>
2020-02-05 17:21:10 +0100

    Kata LF

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LF/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using XamarinFormsKatas.Katas_Logic.Kata_LA;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LF
+{
+    public class MainViewModel:BindableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+        private Deportista _deport = new Deportista(25, 48, "Francisco", "Baloncesto");
+        public Deportista deport
+        {
+            get => _deport;
+            set
+            {
+                _deport = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
