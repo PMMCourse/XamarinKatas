b5d80c62236f7aa6d45587b67ffcb317aa69f579
Natalia <Natalia@NATALIA-HP>
2020-02-05 16:59:57 +0100

    Kata LE

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LE/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinFormsKatas.Katas_Logic.Kata_LA;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LE
+{
+    public class MainViewModel:BaseViewModel
+    {
+        private Command _colorCommando;
+        public ICommand colorCommando => _colorCommando;
+
+        private string _colorRed;
+        public string colorRed
+        {
+            get => _colorRed;
+            set
+            {
+                _colorRed = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _colorGreen;
+        public string colorGreen
+        {
+            get => _colorGreen;
+            set
+            {
+                _colorGreen = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _colorBlue;
+        public string colorBlue
+        {
+            get => _colorBlue;
+            set
+            {
+                _colorBlue = value;
+                OnPropertyChanged();
+            }
+        }
+        private Color _colorMexcla;
+        public Color colorMexcla
+        {
+            get => _colorMexcla;
+            set
+            {
+                _colorMexcla = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            _colorCommando = new Command(CambiarColor);
+        }
+
+        public  void CambiarColor(object obj)
+        {
+            ColorTypeConverter conversion = new ColorTypeConverter();
+            Color Red = Color.Transparent;
+            Color Blue = Color.Transparent;
+            Color Green = Color.Transparent;
+
+            try
+            {
+                Red = (Color)(conversion.ConvertFromInvariantString(colorRed));
+            }
+            catch (InvalidOperationException)
+            {
+                
+            }
+
+            try
+            {
+                Blue = (Color)(conversion.ConvertFromInvariantString(colorBlue));
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+            try
+            {
+                Green = (Color)(conversion.ConvertFromInvariantString(colorGreen));
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+            colorMexcla = MetodoExtencion.Mexcla(Red, Green, Blue);
+        }
+    }
+}
---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LE/MetodoExtencion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LE
+{
+    public static class MetodoExtencion
+    {
+        public static Color Mexcla(this Color c1,Color c2,Color c3)
+        {
+            byte b = (byte)(c1.A + c2.A + c3.A);
+            byte b2 = (byte)(c1.R);
+            byte b3 = (byte)(c1.G);
+            byte b4 = (byte)(c1.B);
+            return Color.FromArgb(b, b2, b3, b4) ;
+        }
+    }
+}
