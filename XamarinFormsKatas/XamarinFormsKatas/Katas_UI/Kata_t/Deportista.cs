e0a3758da60dd16d0c4e8e50e02b1a87882adc29
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-02-05 21:24:22 +0100

    kata t

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/Deportista.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_t
+{
+    public class Deportista
+    {
+        public int Puntuacion { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public string Deporte { get; set; }
+
+        public Deportista(int puntuacion, int edad, string sexo, string deporte)
+        {
+            Puntuacion = puntuacion;
+            Edad = edad;
+            Sexo = sexo;
+            Deporte = deporte;
+        }
+        public Deportista()
+        {
+
+        }
+
+        public override string ToString()
+        {
+
+          return $"Puntuacion: {Puntuacion}, Edad: {Edad}, Sexo: {Sexo}, Deporte: {Deporte}";
+
+        }
+
+    }
+}
--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/KataT.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/KataT.xaml.cs
@@ -12,9 +12,18 @@ namespace XamarinFormsKatas.Katas_UI.Kata_t
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataT : ContentPage
 	{
+		public List<Deportista> deporList = new List<Deportista>();
 		public KataT ()
 		{
 			InitializeComponent ();
+
+			var d1 = new Deportista() { Deporte = "Voleyball", Sexo = "No binario", Puntuacion = 1, Edad = 19 };
+			var d2 = new Deportista() { Deporte = "Baloncesto", Sexo = "Hombre blanco Cis genero", Puntuacion = 3, Edad = 18 };
+
+			deporList.Add(d1);
+			deporList.Add(d2);
+
+			listview.ItemsSource = deporList;
 		}
 	}
 }
