d3110cd91d1392da46bfd8a95cd8dd2e5bed110b
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-02-05 22:10:11 +0100

    kata y, z

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_y/KataY.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_y/KataY.xaml.cs
@@ -12,9 +12,65 @@ namespace XamarinFormsKatas.Katas_UI.Kata_y
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataY : ContentPage
 	{
+		public List<string> lista = new List<string>();
 		public KataY ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            lista.Add("pepe");
+            lista.Add("paco");
+            lista.Add("maria");
+            lista.Add("Sara");
+            lista.Add("Carolina");
+            lista.Add("Nerea");
+            lista.Add("Lucia");
+            lista.Add("Ana");
+            lista.Add("Alba");
+            lista.Add("Tania");
+            lista.Add("Demetrio");
+            lista.Add("Chema");
+            lista.Add("Nazar");
+            lista.Add("Irene");
+            lista.Add("Paula");
+            lista.Add("Ana U");
+            lista.Add("Alex");
+            lista.Add("luis");
+            lista.Add("luisita");
+            lista.Add("amaya");
+
+            Grid nuevoGrid = new Grid()
+            {
+
+            };
+            nuevoGrid.RowDefinitions = new RowDefinitionCollection();
+            nuevoGrid.ColumnDefinitions = new ColumnDefinitionCollection();
+
+            for (int i = 0; i < 3; i++)
+            {
+                nuevoGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                nuevoGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            sl.Children.Add(nuevoGrid);
+            int contador = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (contador < 20)
+                    {
+                        nuevoGrid.Children.Add(new Label
+                        {
+                            Text = lista[contador],
+                            HorizontalOptions = LayoutOptions.Center
+                        }, j, i);
+                        contador++;
+                    }
+                }
+            }
+        }
+
+    }
 }
--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_z/KataZ.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_z/KataZ.xaml.cs
@@ -12,9 +12,65 @@ namespace XamarinFormsKatas.Katas_UI.Kata_z
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class KataZ : ContentPage
     {
+        public List<String> lista = new List<string>();
         public KataZ()
         {
             InitializeComponent();
+            lista.Add("pepe");
+            lista.Add("paco");
+            lista.Add("maria");
+            lista.Add("Sara");
+            lista.Add("Carolina");
+            lista.Add("Nerea");
+            lista.Add("Lucia");
+            lista.Add("Ana");
+            lista.Add("Alba");
+            lista.Add("Tania");
+            lista.Add("Demetrio");
+            lista.Add("Chema");
+            lista.Add("Nazar");
+            lista.Add("Irene");
+            lista.Add("Paula");
+            lista.Add("Ana U");
+            lista.Add("Alex");
+            lista.Add("luis");
+            lista.Add("luisita");
+            lista.Add("amaya");
+
+            Grid nuevoGrid = new Grid
+            {
+                BackgroundColor = Color.Blue,
+                HeightRequest = 500,
+                WidthRequest = 500
+            };
+            nuevoGrid.RowDefinitions = new RowDefinitionCollection();
+            nuevoGrid.ColumnDefinitions = new ColumnDefinitionCollection();
+            for (int i = 0; i < 7; i++)
+            {
+                nuevoGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                nuevoGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            sl.Children.Add(nuevoGrid);
+            int contador = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (contador < 20)
+                    {
+                        nuevoGrid.Children.Add(new Label
+                        {
+                            Text = lista[contador],
+                            FontSize = 25,
+                            HorizontalOptions = LayoutOptions.Center
+                        }, j, i);
+                        contador++;
+                    }
+                }
+            }
         }
     }
 }
