8ee053481f8df6e8b4f8a1b555a391efaf0f20dd
AleRomero2 <alejandro.romero.rodriguez2@gmail.com>
2020-02-05 14:02:56 +0100

    Kata LJ

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LJ/View/Kata_LJ.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormsKatas.Katas_Logic.Kata_LJ.ViewModel;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LJ
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Kata_LJ : ContentPage
+	{
+		public Kata_LJ ()
+		{
+			InitializeComponent ();
+            BindingContext = new MainViewModel();
+        }
+        private string user;
+        private string pass;
+
+        
+
+        private void Accept_Clicked(object sender, EventArgs e)
+        {
+            Error.IsVisible = false;
+            user = User.Text;
+            pass = Pass.Text;
+            if (string.IsNullOrEmpty(user))
+            {
+                Error.Text = "Rellena del campo usuario";
+                Error.IsVisible = true;
+            }
+
+            if (string.IsNullOrEmpty(pass))
+            {
+                Error.Text = "Rellena del campo contraseña";
+                Error.IsVisible = true;
+            }
+        }
+    }
+}
---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LJ/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LJ.ViewModel.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetChanged<T>(ref T refValue, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
