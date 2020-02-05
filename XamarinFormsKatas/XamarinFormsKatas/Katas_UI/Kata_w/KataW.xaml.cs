7721eb80d7abd5d1d1d25062f2b13e9d87d1b649
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-02-05 21:54:06 +0100

    kata w

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
@@ -9,12 +9,36 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinFormsKatas.Katas_UI.Kata_w
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class KataW : ContentPage
-	{
-		public KataW ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KataW : ContentPage
+    {
+        public KataW()
+        {
+            InitializeComponent();
+        }
+        private void clickBoton(object sender, EventArgs e)
+        {
+            EControl.IsVisible = false;
+            LControl.IsVisible = false;
+            BControl.IsVisible = false;
+            AControl.IsVisible = false;
+            if (switchE.IsToggled)
+            {
+                EControl.IsVisible = true;
+            }
+            if (switchL.IsToggled)
+            {
+                LControl.IsVisible = true;
+            }
+            if (switchB.IsToggled)
+            {
+                BControl.IsVisible = true;
+            }
+            if (switchA.IsToggled)
+            {
+                AControl.IsVisible = true;
+            }
+            Navigation.PushAsync(new Katas_UI.Kata_w.ElementControl1(EControl, LControl, BControl, AControl));
+        }
+    }
 }
